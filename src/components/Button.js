010495b1034171f851722e9c592c72ab7d8bcc64
lilbex <47287618+lilbex@users.noreply.github.com>
2022-07-27 22:46:23 +0100

    update singup

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,52 +1,78 @@
-import React from 'react';
-import styled from "styled-components"
-import ClipLoader from "react-spinners/ClipLoader"
+import React from "react";
+import styled from "styled-components";
+import ClipLoader from "react-spinners/ClipLoader";
 
-const MyButton = ({width, children, white, href, min, max, full, className, type, transparent, green, onClick, loading}) => {
-
-    return (
-           <StyledButton width={width} className={className || "btn btn-component"} white={white} green={green} min={min} full={full} type={type} max={max} onClick={onClick}>
-            {children}
-            <div className="spin-parent">
-              {loading && <ClipLoader color="white" size="40px" className="spinner" />}
-            </div>
-            </StyledButton> 
-    )
-}
+const MyButton = ({
+  width,
+  children,
+  white,
+  href,
+  min,
+  max,
+  full,
+  className,
+  type,
+  transparent,
+  green,
+  onClick,
+  loading,
+}) => {
+  return (
+    <StyledButton
+      width={width}
+      className={className || "btn btn-component"}
+      white={white}
+      green={green}
+      min={min}
+      full={full}
+      type={type}
+      max={max}
+      onClick={onClick}
+    >
+      {children}
+      <div className="spin-parent">
+        {loading && (
+          <ClipLoader color="white" size="40px" className="spinner" />
+        )}
+      </div>
+    </StyledButton>
+  );
+};
 const StyledButton = styled.button`
-    color: white;
-    text-decoration: none;
-    letter-spacing: 1px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 0;
-    width: 100%;
-    height: ${({height}) => height ? height : '3.2rem'};
-    font-weight: 400;
-    font-size: 16px;
-    border: none;
-    text-align: center;
-    color: ${({white, transparent}) => white ? "#34A853": transparent ? "#34A853" : "white"};
-    background: linear-gradient(96.67deg, #34A853 0%, #B8D344 100%);
-    background: #14A800;
-    white-space: nowrap;
-    border: ${({transparent}) => transparent ? "1px solid #34A853" : "none"};
-    :hover{
-        cursor: pointer;
-    }
-  
-    .spin-parent{
+  color: white;
+  text-decoration: none;
+  letter-spacing: 1px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0;
+  width: 100%;
+  height: ${({ height }) => (height ? height : "3.2rem")};
+  font-weight: 400;
+  font-size: 16px;
+  border: none;
+  text-align: center;
+  color: ${({ white, transparent }) =>
+    white ? "#34A853" : transparent ? "#34A853" : "white"};
+  background: linear-gradient(96.67deg, #34a853 0%, #b8d344 100%);
+  background: #14a800;
+  white-space: nowrap;
+  border: ${({ transparent }) => (transparent ? "1px solid #34A853" : "none")};
+  :hover {
+    cursor: pointer;
+  }
+
+  .spin-parent {
     position: absolute;
   }
-  
-    @media only screen and (max-width: 600px) {
-        font-size:12px;
-        height: 4.2rem;
 
-    }
-   
-
+  @media only screen and (max-width: 600px) {
+    font-size: 12px;
+    height: 4.2rem;
+  }
+  @media only screen and (max-width: 540px) {
+    max-height: 2rem;
+  }
 `;
 
-export default MyButton
+export default MyButton;
--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 import { format } from "date-fns";
 // import CurrencyInput from "react-currency-input";
 
-const FormInputComponent = ({ height, label, type, name, value, onChange, onBlur, error, placeholder, defaultValue, max, prefix, allowDecimals, disabled,  }) => {
+const FormInputComponent = ({
+  height,
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  onBlur,
+  error,
+  placeholder,
+  defaultValue,
+  max,
+  prefix,
+  allowDecimals,
+  disabled,
+}) => {
   let maxDate = max && format(max, "yyyy-MM-dd");
   // const allowdecimals = allowDecimals;
   return (
@@ -11,75 +26,77 @@ const FormInputComponent = ({ height, label, type, name, value, onChange, onBlur
       {label && (
         <label htmlFor={name} className="form-font form-label">
           {label}
-        </label>)}
-        <input
-          type={type}
-          id={name}
-          max={max && type === "date" ? maxDate : null}
-          placeholder={placeholder}
-          name={name}
-          onChange={onChange}
-          pattern={type==="number" ?  "[0-9]*" : null}
-          onBlur={onBlur}
-          onWheel={type === "number" ? (e) => e.target.blur() : undefined}
-          value={value || defaultValue || ""}
-          className="form-font"
-          disabled={disabled}
-          height={height}
-        />
+        </label>
+      )}
+      <input
+        type={type}
+        id={name}
+        max={max && type === "date" ? maxDate : null}
+        placeholder={placeholder}
+        name={name}
+        onChange={onChange}
+        pattern={type === "number" ? "[0-9]*" : null}
+        onBlur={onBlur}
+        onWheel={type === "number" ? (e) => e.target.blur() : undefined}
+        value={value || defaultValue || ""}
+        className="form-font"
+        disabled={disabled}
+        height={height}
+      />
       {error && <div className="form-error">{error}</div>}
-     
     </StyledFormInputComponent>
   );
-}
+};
 
 const StyledFormInputComponent = styled.div`
-    margin-bottom: 2.4rem;
-    input {
-        display: inline-block;
-        font-size: 1.6rem;
-        background: transparent;
-        -webkit-appearance: none;
-        min-width: 100%;
-        border: none;
-        height: ${({height}) => height ? height : '3.2rem'};
-        padding-bottom: 0rem;
-        padding-left: 1rem;
-        border: 1px solid rgba(33, 51, 79, 0.15) !important;
-    }
-    /* style placeholder */
-    input::-webkit-input-placeholder {
-        color: rgba(33, 51, 79, 0.5);
-        font-size: 1rem;
-    }
-    input[type=number]::-webkit-inner-spin-button, 
-    input[type=number]::-webkit-outer-spin-button { 
-    -webkit-appearance: none !important; 
-    margin: 0; 
-    }
-input[type=number] {
-  -moz-appearance: textfield;
-}
-.form-error {
-  color: red;
-}
-input:disabled{
-background: rgba(0,0,0,0.1);
-}
+  margin-bottom: 2.4rem;
+  input {
+    display: inline-block;
+    font-size: 1.6rem;
+    background: transparent;
+    -webkit-appearance: none;
+    min-width: 100%;
+    border: none;
+    height: ${({ height }) => (height ? height : "3.2rem")};
+    padding-bottom: 0rem;
+    padding-left: 1rem;
+    border: 1px solid rgba(33, 51, 79, 0.15) !important;
+    font-size: 1rem;
+  }
+  /* style placeholder */
+  input::-webkit-input-placeholder {
+    color: rgba(33, 51, 79, 0.5);
+    font-size: 1rem;
+  }
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none !important;
+    margin: 0;
+  }
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+  .form-error {
+    color: red;
+  }
+  input:disabled {
+    background: rgba(0, 0, 0, 0.1);
+  }
 
-input:active{
-  border: 1px solid green !important;
-}
-input:focus{
-  border: 1px solid #34a853 !important;
-}
-    @media only screen and (max-width: 405px) {
-        input{
-            min-width: 100%;
-        }
+  input:active {
+    border: 1px solid green !important;
+  }
+  input:focus {
+    border: 1px solid #34a853 !important;
+  }
+  label{
+    margin-bottom:-5px;
+  }
+  @media only screen and (max-width: 405px) {
+    input {
+      min-width: 100%;
     }
-    
-    
+  }
 `;
 
-export default FormInputComponent
+export default FormInputComponent;
