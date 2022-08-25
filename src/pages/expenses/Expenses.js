234b752d8359f4aae99798a1fcdf904cfd0a2f25
lilbex <47287618+lilbex@users.noreply.github.com>
2022-08-25 10:47:40 +0100

    add page title to each page

--- src/pages/expenses/Expenses.js
+++ src/pages/expenses/Expenses.js
@@ -7,6 +7,7 @@ import MyButton from "../../components/Button";
 import ClipLoader from "react-spinners/ClipLoader";
 import FormTitleSection from "../../components/modal/FormTitleSection";
 import CurrencyFormat from "react-currency-format";
+import Pagination from "../../utils/pagination";
 // import useDialog from "../../hooks/useDialog";
 import FormModal from "../../components/modal/FormModal";
 // import { useFormik } from "formik";
@@ -29,12 +30,17 @@ const expenses = [
   },
 ];
 
+let pageSize = 5;
 const BudgetCategory = () => {
   const [idOfBudget, setIdOfBudget] = useState(-1);
-  const [editModal,setEditModal] = useState(false)
+  const [editModal, setEditModal] = useState(false);
   // eslint-disable-next-line
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  // eslint-disable-next-line
+  const [currentTableData, setCurrentTableData] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   console.log(data);
 
   const ref = useRef(null);
@@ -50,19 +56,32 @@ const BudgetCategory = () => {
       Authorization: "Bearer " + localStorage.getItem("token"),
     },
   };
-  
-  
-  
-// eslint-disable-next-line 
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const budgetId = urlParams.get("budgetId");
+  const catId = urlParams.get("catId");
+
+  // eslint-disable-next-line
   const fetchData = async () => {
     try {
-      const response = await request.get(`budget_categories`, headers);
+      const response = await request.get(
+        `budgets/${budgetId}/lineItems/${catId}/expenses?size=${pageSize}&page=${currentPage}`,
+        headers
+      );
       setData(response.data.data);
+      setCurrentTableData(response.data.data.content);
+      setTotalCount(response.data.data.totalElements);
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
     }
   };
+
+  useEffect(() => {
+   fetchData();
+     // eslint-disable-next-line
+  }, []);
+  console.log(data);
 
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
@@ -75,6 +94,14 @@ const BudgetCategory = () => {
   return (
     <Layout>
       <ListStyle>
+        <div className="header-wrapper">
+          <div className="header">
+            <p style={{ fontWeight: "bold", fontSize: "20px" }}>Expenses</p>
+          </div>
+          <div className="button-container">
+            <button>Create budget</button>
+          </div>
+        </div>
         <div className="category-container">
           <div className="category header">
             <p className="category-title">Description</p>
@@ -90,16 +117,18 @@ const BudgetCategory = () => {
                 <p className="category-title">{item.amount}</p>
                 <p className="category-title">{item.date}</p>
                 <p className="category-title">{item.time}</p>
-                <p
-                  onClick={() => setIdOfBudget(index)}
-                 className="dots"
-                >
+                <p onClick={() => setIdOfBudget(index)} className="dots">
                   ...
                   {idOfBudget === index ? (
                     <Fragment>
                       <span ref={ref} className="popup">
-                        <p onClick={()=>setEditModal(true)} >Edit</p>
-                        <p style={{ color: "red" }}>Delete</p>
+                        <p
+                          className="pop-item"
+                          onClick={() => setEditModal(true)}
+                        >
+                          Edit
+                        </p>
+                        <p className="pop-item delete">Delete</p>
                       </span>
                     </Fragment>
                   ) : null}
@@ -117,7 +146,7 @@ const BudgetCategory = () => {
                 title={`Edit Expenses`}
                 onClick={() => setEditModal(!editModal)}
               />
-              <form 
+              <form
               // onSubmit={onSubmitEdit}
               >
                 <div className="form__wrapper">
@@ -149,6 +178,15 @@ const BudgetCategory = () => {
             </div>
           </FormModal>
         )}
+        <div className="pagination-container">
+          <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={totalCount}
+            pageSize={pageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        </div>
       </ListStyle>
     </Layout>
   );
@@ -158,17 +196,45 @@ export default BudgetCategory;
 
 const ListStyle = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
+  
+  .header-wrapper {
+    width: 100%;
+    display: flex;
+    margin-top:20px;
+    flex-direction: row;
+    justify-content: space-between;
+  }
   .button-container {
     width: 100%;
     display: flex;
     justify-content: flex-end;
   }
-  /* .btn{
-    width:200px;
-  } */
+  button {
+    color: white;
+    text-decoration: none;
+    letter-spacing: 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0;
+    width: 200px;
+    height: 2.5rem;
+    font-weight: 400;
+    font-size: 16px;
+    border: none;
+    text-align: center;
+    color: white;
+    background: #14a800;
+    white-space: nowrap;
+    border: none;
+    :hover {
+      cursor: pointer;
+      background: #14a800;
+    }
+  }
+
   .category-container {
     display: flex;
     flex-direction: column;
@@ -182,8 +248,7 @@ const ListStyle = styled.div`
   .category {
     font-family: "Inter";
     font-style: normal;
-    
-    
+
     width: 100%;
     align-items: center;
     padding: 10px 14px;
@@ -193,23 +258,20 @@ const ListStyle = styled.div`
     gap: 10px;
     /* border-radius:5px; */
   }
-  .header{
+  .header {
     font-weight: 600;
-    margin-bottom:-20px ;
+    margin-bottom: -20px;
     font-size: 16px;
   }
-  .body{
+  .body {
     background: rgba(0, 156, 244, 0.05);
     font-weight: 500;
     font-size: 14px;
   }
 
-  
   .popup {
     position: absolute;
     min-width: 150px;
-    /* right: 20px; */
-    /* top: 120px; */
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -226,71 +288,77 @@ const ListStyle = styled.div`
     font-size: 14px;
     line-height: 19px;
     color: #071232;
+  }
 
-    p:not(:last-child) {
-      margin-bottom: 12px !important;
-    }
-    p:hover {
-      display: inline-block;
-    }
-
-    p {
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
+  .pop-item {
+    cursor: pointer;
+  }
+  .delete {
+    color: red;
   }
   .dots {
-    font-size: 30px; 
-    cursor: "pointer";
-    font-weight:"bold" 
+    font-size: 30px;
+    cursor: pointer;
+    font-weight: "bold";
   }
   @media only screen and (max-width: 990px) {
     .category {
-      padding: 5px 8px ;
-      height:100px;
-  }
-}
-@media only screen and (max-width: 487px) {
-    .category {
-      padding: 4px ;
-      height:150px;
-      font-size:1rem;
-  }
-      .dots{
-        font-size:25px;
-      }
-    .header{
-      font-size:12px;
-    }
-    .body{
-      font-size:12px;
+      padding: 5px 8px;
+      height: 100px;
     }
   }
-@media only screen and (max-width: 377px) {
+  @media only screen and (max-width: 487px) {
     .category {
-     display:flex;
-     justify-content: space-between;
-     width:100%;
-     padding:0px;
-     font-size:12px;
+      padding: 4px;
+      height: 150px;
+      font-size: 1rem;
+    }
+    .dots {
+      font-size: 25px;
+    }
+    .header {
+      font-size: 12px;
+    }
+    .body {
+      font-size: 12px;
+    }
   }
-}
-@media only screen and (max-width: 290px) {
+  @media only screen and (max-width: 377px) {
     .category {
-     display:flex;
-     justify-content: space-between;
-     width:100%;
-     padding:0px;
-     font-size:10px;
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      padding: 0px;
+      font-size: 12px;
+    }
   }
-}
-.form__wrapper {
+  @media only screen and (max-width: 290px) {
+    .category {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      padding: 0px;
+      font-size: 10px;
+    }
+  }
+  .form__wrapper {
     width: 100%;
     border-radius: 5px;
     margin-top: 20px;
   }
   .btn-wrapper {
     margin-top: 20px;
+  }
+  .pagination-container {
+    display: flex;
+    /* flex-direction: column; */
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+  }
+  .pagination-bar {
+    display: flex;
+    justify-content: space-between;
+    width: 300px;
   }
 `;
