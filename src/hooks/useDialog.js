5473e396bd8ea88da2f01da8a235b29f326de859
lilbex <47287618+lilbex@users.noreply.github.com>
2022-08-28 19:10:13 +0100

    delete expenses

--- src/hooks/useDialog.js
+++ src/hooks/useDialog.js
@@ -7,7 +7,43 @@ import { toast } from "react-toastify";
  */
 
 export default function useDialog() {
-  
+  const deleteItemId = (handleDelete, id) => {
+    swal({
+      title: "Are you sure",
+      text: `You want to delete? process cannot be reversed`,
+      icon: "warning",
+      buttons: {
+        none: {
+          text: "cancel",
+          className: "cancel",
+          value: null,
+        },
+        confirm: {
+          text: "delete",
+          value: true,
+          closeModal: false,
+          className: "confirm--delete",
+        },
+      },
+    }).then((value) => {
+      if (value) {
+        handleDelete(id)
+          .then(() => {
+            swal.stopLoading();
+            swal.close();
+            toast.success("Deleted successfully");
+          })
+          .catch((err) => {
+            swal.stopLoading();
+            swal.close();
+            toast.error(err?.response?.data?.message || "Could not delete");
+          });
+      } else {
+        swal.stopLoading();
+        swal.close();
+      }
+    });
+  };
   const deleteItem = (handleDelete, name) => {
     swal({
       title: `Are you sure you want to remove ${name}?`,
@@ -47,8 +83,10 @@ export default function useDialog() {
       }
     });
 };
+
   return {
     deleteItem,
+    deleteItemId
   };
 
 }
