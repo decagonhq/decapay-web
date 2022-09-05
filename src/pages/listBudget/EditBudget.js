3254b94832b8656b2bb43f839fdd3056d3d31fef
Nnabue Favour <nnabuekassidy@gmail.com>
2022-09-05 19:15:28 +0100

    moved changeDateFormat for edit budget to constant file

--- src/pages/listBudget/EditBudget.js
+++ src/pages/listBudget/EditBudget.js
@@ -147,10 +147,7 @@ const EditBudget = ({ closeModal, id, title }) => {
   const token = localStorage.getItem("token");
 
   
-  const changeDateFormat = (date) => {
-    const splitDate = date.split("-");
-    return `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
-  };
+
  
   const onSubmit = async () => {
     
