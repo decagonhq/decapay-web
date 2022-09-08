8e0b43ba3a862c6750af593fbf6a07175761964c
Nnabue Favour <nnabuekassidy@gmail.com>
2022-09-08 02:27:14 +0100

    create landing homepage for users to land on first

--- src/components/NavigationBar/SideMenu.js
+++ src/components/NavigationBar/SideMenu.js
@@ -45,7 +45,7 @@ const MenuStyle = styled.div`
   background: #FFFFFF;
   /* background: #B2BEB5; */
   display: flex;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
   ::-webkit-scrollbar {
     display: none;
   }
