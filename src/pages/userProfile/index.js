5d6c3929096f0eaa822b3a66fcacc87573a8bf1b
Nnabue Favour <nnabuekassidy@gmail.com>
2022-09-19 07:44:42 +0100

    consummed get user details api

--- src/pages/userProfile/index.js
+++ src/pages/userProfile/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Button from "../../components/Button";
 import FormInputComponent from "../../components/InputComponent";
@@ -7,6 +7,8 @@ import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import registerUser from "../../redux/actions/auth/signup.action";
 import ClipLoader from "react-spinners/ClipLoader";
+import Select from "react-select";
+import request from "../../utils/apiHelper";
 // Validation for profile update
 
 const phoneRegExp = /^\d*(\+\d+)?$/;
@@ -28,20 +30,118 @@ const phoneRegExp = /^\d*(\+\d+)?$/;
 const Home = () => {
   
   const [loading, setLoading] = useState(false);
+  const [countryOptions, setCountryOptions] = useState([]);
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [languageOptions, setLanguageOptions] = useState([]);
+  const [language, setLanguage] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [country, setCountry] = useState("");
 
   // eslint-disable-next-line
   const [disabled, setDisabled] = useState(true);
-// console.log(countryOptions);
+  const [countryCodeError, setCountryCodeError] = useState(
+    "Country is required"
+  );
+  const [currencyCodeError, setCurrencyCodeError] = useState(
+    "Currency code is required"
+  );
+  const [languageCodeError, setLanguageCodeError] = useState(
+    "Language Code is required"
+  );
+  const getUser = async () => {
+    try {
+      const response = await request.get(`user`);
+      let promisefulfilled = response.data.data;
+      console.log(promisefulfilled);
+      console.log(promisefulfilled);
+      formik.setFieldValue("firstName", promisefulfilled.firstName);
+      formik.setFieldValue("lastName", promisefulfilled.lastName);
+      formik.setFieldValue("email", promisefulfilled.email);
+      formik.setFieldValue("phoneNumber", promisefulfilled.phoneNumber);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    getReferences();
+    // eslint-disable-next-line
+    getUser();
+    // eslint-disable-next-line
+  }, []);
+
+  const getReferences = async () => {
+    try {
+      const response = await request.get("references");
+      let promisefulfilled = response.data.data;
+      let country = promisefulfilled.countries.map((country) => ({
+        value: country.code,
+        label: country.name,
+      }));
+      country.unshift({ value: "", label: "Select country" });
+      setCountryOptions(country);
+
+      let currency = promisefulfilled.currencies.map((currency) => ({
+        value: currency.code,
+        label: currency.name,
+      }));
+      currency.unshift({ value: "", label: "Select currency" });
+      setCurrencyOptions(currency);
+      let language = promisefulfilled.languages.map((language) => ({
+        value: language.code,
+        label: language.name,
+      }));
+      language.unshift({ value: "", label: "Select language" });
+      setLanguageOptions(language);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log(countryOptions);
+ 
+ 
+  const languageChange = (value) => {
+    if (value) {
+      setLanguage(value);
+      setLanguageCodeError("");
+    }
+    setLanguage(value);
+  };
+  const currencyChange = (value) => {
+    if (value) {
+      setCurrency(value);
+      setCurrencyCodeError("");
+    }
+  };
+  const countryChange = (value) => {
+    if (value) {
+      setCountry(value);
+      setCountryCodeError("");
+    }
+  };
   const dispatch = useDispatch();
   const initialValues = {
     email: "",
     lastName: "",
     firstName: "",
     phoneNumber: "",
+    countryCode: "",
+    currencyCode: "",
+    languageCode: "",
   };
   const onSubmit = (values) => {
+    if (
+      country.value === "" ||
+      currency.value === "" ||
+      language.value === ""
+    ) {
+      return;
+    }
     setLoading(true);
+    values.countryCode = country.value;
+    values.currencyCode = currency.value;
+    values.languageCode = language.value;
     delete values.confirmPassword;
     dispatch(registerUser(values));
     setLoading(false);
@@ -106,6 +206,46 @@ const Home = () => {
                   />
                 </div>
               </div>
+
+              <div className="form__wrapper padding">
+                <label>Country</label>
+                <Select
+                  options={countryOptions}
+                  value={country}
+                  onChange={countryChange}
+                  name="countryCode"
+                  className="select"
+                />
+                {country.value === "" && (
+                  <p className="error">{countryCodeError}</p>
+                )}
+              </div>
+              <div className="form__wrapper padding">
+                <label>Language</label>
+                <Select
+                  name="languageCode"
+                  options={languageOptions}
+                  value={language}
+                  onChange={languageChange}
+                  className="select"
+                />
+                {language.value === "" && (
+                  <p className="error">{languageCodeError}</p>
+                )}
+              </div>
+              <div className="form__wrapper padding">
+                <label>Currency</label>
+                <Select
+                  name="currencyCode"
+                  options={currencyOptions}
+                  value={currency}
+                  onChange={currencyChange}
+                  className="select"
+                />
+                {currency.label === "" && (
+                  <p className="error">{currencyCodeError}</p>
+                )}
+              </div>
               <div className="form__wrapper padding">
                 <Button disabled={disabled} onClick={formik.handleSubmit} type="submit">
                   {loading ? <ClipLoader color="white" size="40px" /> : "Save"}
@@ -120,7 +260,6 @@ const StyledHome = styled.div`
   .two-column {
     width: 100%;
     display: flex;
-    flex-direction: column;
     gap: 10px;
   }
 
